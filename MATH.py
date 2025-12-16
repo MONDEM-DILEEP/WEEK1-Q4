9de24090a4f8971c66eca02c815a319c69872cb5
MONDEM-DILEEP <24331a05i4@mvgrce.edu.in>
2025-12-16 09:21:01 +0530

    Add files via upload

---
+++ MATH.py
@@ -0,0 +1,11 @@
+
+import math
+print("pi value:", math.pi)
+
+val = 36
+print("square root of", val, "is", math.sqrt(val))
+
+ang = 180
+print("for cos", ang, "is", math.cos(ang))
+print("for sin", ang, "is", math.sin(ang))
+
